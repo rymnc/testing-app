827bb4b05ed74cf185c3e1cdaeb10ac1fc252f69
aaryamannchallani <aaryamannchallani7@gmail.com>
2020-06-21 15:36:34 +0530

    Refactored index,context-provider

--- my-app/src/Context-Provider.jsx
+++ my-app/src/Context-Provider.jsx
@@ -1,34 +1,31 @@
-import React, {createContext} from 'react';
-import Axios from 'axios'
-import {useState,useEffect} from 'react'
+import React, { createContext } from "react";
+import Axios from "axios";
+import { useState, useEffect } from "react";
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
-export const AppProvider = (props) =>{
+export const AppProvider = (props) => {
+  const [data, setData] = useState([]);
 
-    const [data,setData] = useState([])
+  useEffect(() => {
+    const fetcher = async () => {
+      const fetching = await Axios.get("http://127.0.0.1:3000/data");
+      console.log(fetching.data);
+      setData(fetching.data);
+    };
 
-    useEffect(() => {
-        const fetcher = async () =>{
-            const fetching = await Axios.get('http://127.0.0.1:3000/data')
-            console.log(fetching.data)
-            setData(fetching.data) 
-        }
+    fetcher();
+  }, []);
 
-        fetcher()
-        
-    }, [])
+  const handleUpdate = async () => {
+    const fetching = await Axios.get("http://127.0.0.1:3000/data");
+    console.log(fetching.data);
+    setData(fetching.data);
+  };
 
-    const handleUpdate = async () =>{
-        const fetching = await Axios.get('http://127.0.0.1:3000/data')
-        console.log(fetching.data)
-        setData(fetching.data)
-    }
-
-
-    return(
-        <AppContext.Provider value={{data,handleUpdate}}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
+  return (
+    <AppContext.Provider value={{ data, handleUpdate }}>
+      {props.children}
+    </AppContext.Provider>
+  );
+};
